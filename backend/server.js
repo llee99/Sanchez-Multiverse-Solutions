2fd2b473a4faf4efaf7b60602b7e667628053f78
llee99 <121249795+llee99@users.noreply.github.com>
2024-10-05 03:21:25 -0500

    Configured environemnt values and placeholders, setup server.js file in backend, fleshed out dir/file structure in frontend and backend.

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app=express();
+
+//Middleware
+app.arguments(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiredTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+// Routes
+const authRoutes = require('./routes/auth');
+const serviceRoutes = require('./routes/services');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/services', serviceRoutes);
+
+// Root Endpoint
+app.get('/', (req, res) => {
+    res.send('Sanchez Multiverse Solutions API');
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
