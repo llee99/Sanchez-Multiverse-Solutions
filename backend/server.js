cbafa979e186167115bcef2254783d1c707bef20
llee99 <121249795+llee99@users.noreply.github.com>
2024-10-07 06:18:22 -0500

    updated server mongo uri and env file

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,11 @@ dotenv.config();
 const app=express();
 
 //Middleware
-app.arguments(cors());
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiredTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
